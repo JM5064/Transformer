d912b5f90766971b830426ca5c1732a129de1625
Justin <thisisjustinmao@gmail.com>
2026-05-06 12:55:25 -0400

    Add parsing of dataset ish

--- data/wikitext2.py
+++ data/wikitext2.py
@@ -8,23 +8,39 @@ class WikiText2(Dataset):
         super().__init__()
 
         if split == 'train':
-            self.df = pd.read_parquet('data/wikitext2/train-00000-of-00001.parquet').to_dict()
+            self.df = pd.read_parquet('data/wikitext2/train-00000-of-00001.parquet')
         elif split == 'val':
-            self.df = pd.read_parquet('data/wikitext2/validation-00000-of-00001.parquet').to_dict()
+            self.df = pd.read_parquet('data/wikitext2/validation-00000-of-00001.parquet')
         else:
-            self.df = pd.read_parquet('data/wikitext2/test-00000-of-00001.parquet').to_dict()
+            self.df = pd.read_parquet('data/wikitext2/test-00000-of-00001.parquet')
+
+        self.data = []
+        self.create_data(min_length=50)
+        
+        for i in range(len(self.data)):
+            print(self.data[i])
+            print()
 
 
-    def clean(self):
-        for i, text in enumerate(self.df.iterrows()):
-            print(text)
-            
-            if i > 5:
-                break
+    def create_data(self, min_length):
+
+        for row in self.df.itertuples():
+            text = row.text
+
+            if len(text) < min_length:
+                continue
+
+            stripped_text = text.strip('\n')
+
+            self.data.append(stripped_text)
+
+
+    def __getitem__(self, index):
+        return self.data[index]
 
 
 if __name__ == "__main__":
     dataset = WikiText2()
 
-    dataset.clean()
+
 
